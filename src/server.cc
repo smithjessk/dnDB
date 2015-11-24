93df3fdc113f0b71113ba131c39668b33cda8cdd
Jess Smith <smith.jessk@gmail.com>
2015-11-24 16:56:49 -0500

    Finish CRUD API for tables

--- src/server.cc
+++ src/server.cc
@@ -9,10 +9,42 @@ int main() {
     return "pong";
   });
 
-  CROW_ROUTE(app, "/table/<string>")
-  .methods("GET"_method)
-  ([](std::string table_name) {
-    return crow::response(table_name);
+  CROW_ROUTE(app, "/table/create")
+  .methods("POST"_method)
+  ([](const crow::request &req) {
+    auto body = crow::json::load(req.body);
+    if (!body) {
+      return crow::response(400); // Bad request
+    }
+    std::string table_name = body["table_name"].s();
+    std::ostringstream os;
+    os << table_name;
+    return crow::response{os.str()};
+  });
+
+  CROW_ROUTE(app, "/table/read")
+  .methods("POST"_method)
+  ([](const crow::request &req) {
+    return crow::response("POST /table/read");
+  });
+
+  CROW_ROUTE(app, "/table/update")
+  .methods("POST"_method)
+  ([](const crow::request &req) {
+    auto body = crow::json::load(req.body);
+    if (!body) {
+      return crow::response(400);
+    }
+    std::string table_name = body["table_name"].s();
+    std::ostringstream os;
+    os << table_name;
+    return crow::response{os.str()};
+  });
+
+  CROW_ROUTE(app, "/table/delete")
+  .methods("DELETE"_method) 
+  ([](const crow::request &req) {
+    return crow::response("DELETE /table/delete");
   });
 
   app.port(8080).multithreaded().run();
