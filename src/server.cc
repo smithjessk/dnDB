b69ee001b34a48ae077f5b37cd3d3a96b51f49dc
Jess Smith <smith.jessk@gmail.com>
2015-11-24 16:18:45 -0500

    Limit HTTP methods on certain routes

--- src/server.cc
+++ src/server.cc
@@ -4,11 +4,13 @@ int main() {
   crow::SimpleApp app;
 
   CROW_ROUTE(app, "/ping")
+  .methods("GET"_method)
   ([] {
     return "pong";
   });
 
   CROW_ROUTE(app, "/table/<string>")
+  .methods("GET"_method)
   ([](std::string table_name) {
     return crow::response(table_name);
   });
