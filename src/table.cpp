ef8e0b2edfec7ca3d4ca8b5aea2d654db91429fa
aazari420 <azarialeriza@gmail.com>
2015-12-01 19:46:43 -0500

    addRow Update
    Table fully (?) working
    implemented addRow

--- src/table.cpp
+++ src/table.cpp
@@ -5,14 +5,14 @@
 #include <unordered_map>
 #include <algorithm>
 #include <fstream>
+#include <sstream>
 
 class Table {
     public:
         int numRows, numCols;
         std::unordered_map<std::string, std::unordered_map<int, std::string> > masterTable;
-        bool is_number(const std::string);
         Table(std::string filepath);
-        void addRow(std::string);
+        //void addRow(std::string); do we need?
         void addRow(int, std::string);
         void addColumn(std::string);
         void removeRow(int);
@@ -20,28 +20,35 @@ class Table {
         int getNumRows();
         int getNumCols();
         std::vector<std::string> getColNames();
+        std::vector<std::string> getRow(int);
 };
 
 bool is_number(const std::string& s) {
     return !s.empty() && std::find_if(s.begin(), s.end(), [](char c) { return !std::isdigit(c); }) == s.end();
 }
 
-void Table::addRow(std::string CSV){ //
-    //GENERATE id, 
-    //call addRow(int, string);
+std::vector<std::string> split(std::string s){
+    std::vector<std::string> result;
+    std::stringstream ss(s);
+    while (!ss.eof()){
+      std::string x;
+      getline( ss, x, ',' );
+      result.push_back(x);
+    }
+    return result;
 }
 
-void Table::addRow(int id, std::string CSV){ //
-    //foo stores parsed CSV
-
-    std::vector<std::string> foo {"rowval1", "rowval2", "rowval3"}; //TEMPORARY. SHOULD BE DERIVED FROM CSVREADER
+void Table::addRow(int id, std::string CSV){
+    std::vector<std::string> rows;
     std::vector<std::string> cols = getColNames();
+    rows=split(CSV);
+
     //FOO MUST BE SORTED BY COLS (Corresponding column to parsed value aka name:"jess", age:"14")
     //THROW EXCEPTION IF SIZE MISMATCH
 
     int cnt=0;
     for(auto k:cols){
-        masterTable[k][id]=foo.at(cnt);
+        masterTable[k][id]=rows.at(cnt);
         cnt++;
     }
 
@@ -72,45 +79,54 @@ int Table::getNumCols(){
 
 std::vector<std::string> Table::getColNames(){
     std::vector<std::string> keys;
-    for(auto k: masterTable){   
+    for(auto k: masterTable){
         keys.push_back(k.first);
     }
     return keys;
 
 }
 
+std::vector<std::string> Table::getRow(int id){
+    std::vector<std::string> row;
+    std::vector<std::string> cols = getColNames();
+    for(auto k:cols){
+        row.push_back(masterTable[k][id]);
+    }
+    return row;
+}
+
 Table::Table(std::string fileName) {
     std::ifstream myFile;
-    
+
     myFile.open(fileName);
-    
+
     if (!myFile) {
         throw "No such file in directory.";
     }
-    
-    
+
+
     std::string line;
     int lineCount = 0;
     std::string header;
-    
+
     while (getline(myFile, line)) {
-        
+
         //find first comma
         int comma = (int)line.find(",");
-        
+
         //if header line
         if (lineCount == 0) {
             //get header string
             header = line.substr(comma + 1);
-            
+
             int columnCount = 0;
-            
+
             while (comma != -1) {
                 comma = (int)line.find(",");
-                
+
                 //string before first comma
                 std::string value = line.substr(0, line.find(","));
-                
+
                 //if first line & first column, should be "_id"
                 if (columnCount == 0) {
                     if (value != "_id") {
@@ -120,7 +136,8 @@ Table::Table(std::string fileName) {
                     }
                 }
                 addColumn(value);
-                
+                //std::cout << "value: " << value << std::endl;
+
                 //remove first string and comma from line for next iteration
                 line = line.substr(line.find(",") + 1);
                 columnCount++;
@@ -133,12 +150,29 @@ Table::Table(std::string fileName) {
                 std::string error = "ERROR: id " + id + " not an int";
                 throw error;
             }
-            
-            int value = std::stoi(id);
-            
-            //IMPLEMENT: addRow(value ,header, line.substr(comma + 1));
+
+            int value;
+            std::stringstream ss;
+            ss<<id;
+            ss>>value;
+            addRow(value, line);
+            //std::cout<<value<<std::endl;
         }
-        
+
         lineCount++;
     }
+}
+
+
+int main(){
+    Table table("table.csv");
+    std::vector<std::string> rows = table.getRow(2);
+    for(std::string s:rows){
+        std::cout<<s<<" ";
+    }
+
+
+
+    return 0;
+
 }
