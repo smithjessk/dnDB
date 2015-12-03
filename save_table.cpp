e29771be3ddedadfc6dd99dadb26d872bd7d982e
Zhihong Sun <zpetersun51@gmail.com>
2015-12-02 19:10:17 -0500

    Update Save Table Method
    Updated the method to comply with the table class

--- save_table.cpp
+++ save_table.cpp
@@ -9,15 +9,7 @@ using namespace std;
 
 void save_table(unordered_map<string, unordered_map<int, string>> table);
 
-int main(){
-	//sample tables
-	unordered_map<int, string> a{{1, "12"}, {2, "19"}, {3, "18"}};
-	unordered_map<int, string> b{{1, "Peter"}, {2, "Jess"}, {4, "Victor"}};
-	unordered_map<string, unordered_map<int, string>> hi{{"age", a}, {"name", b}};
-	save_table(hi);
-	return 0;
-}
-void save_table(unordered_map<string, unordered_map<int, string>> table){
+void save_table(){
 	ofstream save_file;
 	save_file.open("save.csv");
 	if(save_file.is_open()){
@@ -26,10 +18,10 @@ void save_table(unordered_map<string, unordered_map<int, string>> table){
 
 		//save the names of the columns 
 		save_file << "_id,";
-		for(auto colIt = table.begin(); colIt != table.end(); ++colIt){
+		for(auto colIt = masterTable.begin(); colIt != masterTable.end(); ++colIt){
 			save_file << "\"" << colIt->first << "\"";
 			counter++;
-			if(counter < table.size()){
+			if(counter < masterTable.size()){
 				save_file << ",";
 			}else{
 				save_file << endl;
@@ -51,20 +43,20 @@ void save_table(unordered_map<string, unordered_map<int, string>> table){
 		int matchCounter = 0;
 		while(counter < keyValue.size()){
 			save_file << keyValue[indexCounter] << ",";
-			for(auto colIt = table.begin(); colIt != table.end(); ++colIt){
+			for(auto colIt = masterTable.begin(); colIt != masterTable.end(); ++colIt){
 				unordered_map<int, string> row = colIt->second;
 				for(auto rowIt = row.begin(); rowIt != row.end(); ++rowIt){
 					if(rowIt->first == keyValue[indexCounter]){
 						save_file << "\"" << rowIt->second << "\"";
 						matchCounter++;
 					}
-				}
-				//adds a space for empty values
+				
+}				//adds a space for empty values
 				if(matchCounter == 0){
 					save_file << "\" \"";
 				}
 				//add the comma to separate entries
-				if(colIt != --table.end()){
+				if(colIt != --masterTable.end()){
 					save_file << ",";
 				}
 				matchCounter = 0;
