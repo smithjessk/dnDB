214b0914aa0d2c5c4e582f7080d2da64a112cb95
aazari420 <azarialeriza@gmail.com>
2015-12-04 19:34:50 -0500

    error with addcolumn
    Expected output: columns in order "....New Column22,Last Name,New Column"
    Actual out put: out of order            "....,Last Name,New Column,New Column22"
    *disregards quotation marks, dont really matter. can fix easily.

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+int main(){
+    Table table("table.csv");
+    //table.addColumn("\"newColumn\"");
+    //table.addRow(5,"5,\"James\",\"16\",y");
+    //table.addColumn("\"name\"");
+    table.addRow("77,\"James\",\"16\",\"none\"");
+    table.addRow("\"Kevin\",\"44\",\"fries\"");
+    //table.addColumn("New Column22");
+    table.addColumn("New Column22");
+    //table.addColumn("c1");
+    table.addColumn("Last Name");
+    table.addColumn("New Column");
+    table.addRow("\"Jack\",\"54\",\"cucumber\",22,\"Smith\",cc");
+    table.addRow("\"Chad\",\"69\",\"banana\",22,\"Thundercock\",cc");
+
+    //table.addColumn("z");
+    //print(table.getRow(5));
+
+
+    table.save_table("save.csv");
+    return 0;
+}
