a983e0d94a9bd2fb028ed102adcc13755ff3359d
victor-lin <victor-lin@users.noreply.github.com>
2015-12-06 12:36:00 -0500

    Add tests, rename to test_rowmethods
    Added tests for getRow and removeRow

---
+++ test/test_rowmethods.cc
@@ -0,0 +1,146 @@
+#include <iostream>
+#include "table.h"
+
+int main() {
+    
+    Table table = *new Table("./data/table.csv");
+    
+    try {
+        std::cout << "Table name: " << table.getTableName() << std::endl;
+        std::cout << std::endl;
+        
+        std::cout << "Original: " << std::endl;
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with empty string
+    try {
+        std::cout << "Adding row with empty string..." << std::endl;
+        table.addRow("");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with one extra element
+    try {
+        std::cout << "Adding row one extra element..." << std::endl;
+        table.addRow("\"Jack\",\"54\",\"cucumber\",\"extra column\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with an (invalid) empty element
+    try {
+        std::cout << "Adding row an empty element..." << std::endl;
+        table.addRow("\"Jack\",,\"cucumber\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with no given ID
+    try {
+        std::cout << "Adding row with no given ID..." << std::endl;
+        table.addRow("\"Jack\",\"54\",\"cucumber\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with custom ID 6
+    try {
+        std::cout << "Adding same row with custom ID 6..." << std::endl;
+        table.addRow("6,\"Jack\",\"54\",\"cucumber\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with custom ID 6, should throw exception
+    try {
+        std::cout << "Adding same row with custom ID 6..." << std::endl;
+        table.addRow("6,\"Jack\",\"54\",\"cucumber\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //remove row with ID 5
+    try {
+        std::cout << "Remove row with ID 5..." << std::endl;
+        table.removeRow(5);
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //add row with no given ID, ID should be 7
+    try {
+        std::cout << "Adding row with no given ID..." << std::endl;
+        table.addRow("\"Jack\",\"54\",\"cucumber\"");
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //print row with ID 3
+    try {
+        std::cout << "Get row with ID 3..." << std::endl;
+        for (int i = 0; i < table.getRow(3).size(); i++) {
+            std::cout << table.getRow(3)[i];
+            if (i < table.getRow(3).size() - 1) {
+                 std::cout << ", ";
+            }
+        }
+        std::cout << "\n" << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //print row with ID 100
+    try {
+        std::cout << "Get row with ID 100..." << std::endl;
+        for (int i = 0; i < table.getRow(100).size(); i++) {
+            std::cout << table.getRow(100)[i];
+            if (i < table.getRow(100).size() - 1) {
+                std::cout << ", ";
+            }
+        }
+        std::cout << "\n" << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    //remove row with ID 100
+    try {
+        std::cout << "Remove row with ID 100..." << std::endl;
+        table.removeRow(100);
+        std::cout << table.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    try {
+        std::cout << "Saving table into save.csv..." << std::endl;
+        table.saveTable("./data/save.csv");
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+}
