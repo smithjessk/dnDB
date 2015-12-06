027aeca0c995c493b76632433d01468ea68f69b0
victor-lin <victor-lin@users.noreply.github.com>
2015-12-06 12:07:34 -0500

    Add printing for table2
    print table2 filename and original content

--- test/test_constructor.cc
+++ test/test_constructor.cc
@@ -45,6 +45,17 @@ int main() {
     }
     
     try {
+        std::cout << "Table name: " << table2.getTableName() << std::endl;
+        std::cout << std::endl;
+        
+        std::cout << "Original: " << std::endl;
+        std::cout << table2.getSerializedTable() << std::endl;
+    }
+    catch (TableException &cException) {
+        std::cerr << "A table exception occurred (" << cException.getError() << ")\n" << std::endl;
+    }
+    
+    try {
         std::cout << "Adding column \"name\"..." << std::endl;
         table2.addColumn("\"name\"");
         std::cout << table2.getSerializedTable() << std::endl;
